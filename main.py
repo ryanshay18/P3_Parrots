30574a6387f5275b25e4e23edbb7e43078e0bdfd
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2020-12-16 10:12:44 -0800

    fix main

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port='5001')
-
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
 
-@app.route('quiz')
+@app.route('/quiz')
 def quiz():
     return render_template("quiz.html")
 
 
-@app.route('about')
+@app.route('/about')
 def about():
     return render_template("about.html")
 
