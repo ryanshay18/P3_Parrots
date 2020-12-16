1fe4967226ab49aaf1f66bff0d50297540dd734c
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2020-12-16 10:04:32 -0800

    iml

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port='5001')
+
 
 @app.route('/')
 def home():
@@ -19,4 +22,4 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port='3000', host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='3000')
