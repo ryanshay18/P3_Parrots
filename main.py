9d4aeda462df11466e5a351a67a77aeaf5ee44a1
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2020-12-17 09:59:00 -0800

    navbar

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-from flask import Flask, render_template, request
+#import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port='5001')
-
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
 
-@app.route('quiz')
+@app.route('/quiz')
 def quiz():
     return render_template("quiz.html")
 
 
-@app.route('about')
+@app.route('/about')
 def about():
     return render_template("about.html")
 
+"""
+@app.route('/api/')
+def api():
+    headers = {
+        'x-rapidapi-key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
+        'x-rapidapi-host': "university-college-list-and-rankings.p.rapidapi.com"
+    }
+    url = "https://university-college-list-and-rankings.p.rapidapi.com/api/top-universities/europe"
+    response = requests.request("GET", url, headers=headers)
+    setup = response.json()[0]['setup']
+    college = response.json()[0]['college']
+    return render_template('colleges.html', setup = setup, college = college)
+    #print(response.text)
+"""
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port='3000')
