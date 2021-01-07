39cb4a11c71e6ecb007fae6a0e6196f76b4af1e8
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2021-01-07 10:04:49 -0800

    mysql database created

---
+++ database.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import Error, connection
+import pandas as pd
+
+# A function to connect to our MySQL Server
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print("Error: '{err}'")
+
+    return connection
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+def create_database_query():
+    pass
+
+create_database_query = "create database college"
+create_database(connection, create_database_query)
