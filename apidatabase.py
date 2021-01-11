d63234349a1c432f316ac6b0c5d0a3668d0d9286
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2021-01-11 10:09:29 -0800

    api key

--- apidatabase.py
+++ apidatabase.py
@@ -3,7 +3,7 @@ import requests
 url = "https://university-college-list-and-rankings.p.rapidapi.com/api/top-universities/us"
 
 headers = {
-    'x-rapidapi-key': "SIGN-UP-FOR-KEY",
+    'x-rapidapi-key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
     'x-rapidapi-host': "university-college-list-and-rankings.p.rapidapi.com"
 }
 
