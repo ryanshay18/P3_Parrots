5fd054933a90299bce8cf9d61b3ee9a062207825
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2021-01-12 10:12:00 -0800

    python unirest

--- apidatabase.py
+++ apidatabase.py
@@ -1,12 +1,15 @@
-import requests
+import http.client
 
-url = "https://university-college-list-and-rankings.p.rapidapi.com/api/top-universities/us"
+conn = http.client.HTTPSConnection("university-college-list-and-rankings.p.rapidapi.com")
 
 headers = {
     'x-rapidapi-key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
     'x-rapidapi-host': "university-college-list-and-rankings.p.rapidapi.com"
 }
 
-response = requests.request("GET", url, headers=headers)
+conn.request("GET", "/api/top-universities/europe", headers=headers)
 
-print(response.text)
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
