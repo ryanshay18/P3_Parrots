1f21555617f35b11fcb9e0713612a894e7bcfc1a
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2021-01-13 10:11:51 -0800

    lol fixed to us database

--- apidatabase.py
+++ apidatabase.py
@@ -3,11 +3,11 @@ import http.client
 conn = http.client.HTTPSConnection("university-college-list-and-rankings.p.rapidapi.com")
 
 headers = {
-    'x-rapidapi-key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
-    'x-rapidapi-host': "university-college-list-and-rankings.p.rapidapi.com"
+    'x-RapidAPI-key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
+    'x-RapidAPI-host': "university-college-list-and-rankings.p.rapidapi.com"
 }
 
-conn.request("GET", "/api/top-universities/europe", headers=headers)
+conn.request("GET", "/api/top-universities/us", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
