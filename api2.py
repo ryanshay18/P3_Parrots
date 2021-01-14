1b6332a2ce3b24a17629a527f003570ccffcad8f
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2021-01-13 18:59:04 -0800

    1 more api to try, not really sure how to use it yet

--- api2.py
+++ api2.py
@@ -2,11 +2,11 @@ import requests
 
 url = "https://universities-and-colleges.p.rapidapi.com/universities"
 
-querystring = {"page":"5","includeUniversityDetails":"false","countryCode":"US","limit":"50"}
+querystring = {"page": "5", "includeUniversityDetails": "false", "countryCode": "US", "limit": "50"}
 
 headers = {
-    'x-rapidapi-key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
-    'x-rapidapi-host': "universities-and-colleges.p.rapidapi.com"
+    'x-RapidAPI-Key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
+    'x-RapidAPI-Host': "universities-and-colleges.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
---
+++ api3.py
@@ -0,0 +1,15 @@
+import requests
+
+# url = "https://api.data.gov/ed/collegescorecard/v1/schools"
+url = "https://developer.nrel.gov/api/collegescorecard/v1/schools"
+
+# querystring = {"page": "5", "includeUniversityDetails": "false", "countryCode": "US", "limit": "50"}
+
+headers = {
+    'x-Api-Key': "HiLqtF9KVhmYDv91ckwwPCN2i5jje4n9rBFWO9CG",
+    'x-API-Host': "https://developer.nrel.gov"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
