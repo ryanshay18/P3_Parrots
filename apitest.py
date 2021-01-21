d1c5f8c650c00c899dbfced4da43410d8235bc64
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2021-01-20 20:01:57 -0800

    College.html added colleges

--- apitest.py
+++ apitest.py
@@ -1,15 +1,18 @@
 import requests
 
-url = "https://universities-and-colleges.p.rapidapi.com/universities-by-id"
+url = "https://universities-and-colleges.p.rapidapi.com/universities"
 
-querystring = {"id": "<REQUIRED>"}
+querystring = {"page": "1", "includeUniversityDetails": "false", "countryCode": "US", "limit": "50"}
 
 headers = {
-    'x-rapidapi-key': "7a79b8a230mshff68040a0bd2bf0p1174f8jsnf85b178f3369",
-    'x-rapidapi-host': "universities-and-colleges.p.rapidapi.com"
+    'x-RapidAPI-Key': "7060fafea1mshc1031ccdb460c56p1e6e83jsnc95eba373c88",
+    'x-RapidAPI-Host': "universities-and-colleges.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
+list = response.json()
 
+for item in list:
+    print(item["name"])
--- create.py
+++ create.py
@@ -25,7 +25,6 @@ class Users(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=False, nullable=False)
     gpa = db.Column(db.String(255), unique=False, nullable=False)
-    country = db.Column(db.String(60), unique=True, key='countryCode')
 
 
 if __name__ == "__main__":
@@ -33,11 +32,8 @@ if __name__ == "__main__":
     db.create_all()
 
     try:
-        u1 = Users(name='Thomas Edison', gpa='Toby', countryCode='tedison@example.com')
-        u2 = Users(name='Nicholas Tesla', gpa='Niko', countryCode='ntesla@example.com')
-        u3 = Users(name='Alexander Graham Bell', gpa='Lex', countryCode='agbell@example.com')
-        u4 = Users(name='Eli Whitney', gpa='Whit', countryCode='eliw@example.com')
-        session.add_all([u1, u2, u3, u4])
+        u1 = Users(name='Thomas Edison', gpa='Toby')
+        session.add_all([u1])
         session.commit()
 
     except:
@@ -49,4 +45,3 @@ if __name__ == "__main__":
         print(row.user_id)
         print(row.name)
         print(row.gpa)
-        print(row.countryCode)
