3842440dc01b19fb302eaf005a5852c8a6bd638c
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2021-02-10 20:19:47 -0800

    questions in py

--- query.py
+++ query.py
@@ -8,6 +8,6 @@ conn = sqlite3.connect('schools.db')
 
 # def construct_query(region, locale)
 
-for row in conn.execute("select name, city, state, url, locale, carnegie_basic from schools where city = 'San Diego' and carnegie_basic = 30"):
+for row in conn.execute("select name, city, state, url, locale, carnegie_basic from schools where city = 'Denver' and carnegie_basic = 11"):
   print(row)
 
--- quiz.py
+++ quiz.py
@@ -1,14 +1,14 @@
 class Answer:
-  def __init__(self, answer_index, answer_text):
-    self.index = answer_index
-    self.text = answer_text
+    def __init__(self, answer_index, answer_text):
+        self.index = answer_index
+        self.text = answer_text
 
 
 class Question:
-  def __init__(self, index, question, answers):
-    self.index = index
-    self.question = question
-    self.answers = answers
+    def __init__(self, index, question, answers):
+        self.index = index
+        self.question = question
+        self.answers = answers
 
 
 questions = [
@@ -17,21 +17,36 @@ questions = [
               Answer(2, "Suburb (Population: 100,000 - 250,000)"),
               Answer(3, "Town (cluster 10 - 35 miles away from urban area)"),
               Answer(4, "Rural (5 - 25 miles away from urban area)")]),
-    Question(2, "How smart are you?",
-             [Answer(1, "Not very"),
-              Answer(2, "Extremely"),
-              Answer(3, "Who, me?")]),
-    Question(3, "How dumb are you?",
-             [Answer(1, "Not very"),
-              Answer(2, "Extremely"),
-              Answer(3, "Who, me?")])
+    Question(2, "What is your ideal location?",
+             [Answer(1, "New England (CT, ME, MA, NH, RI, VT)"),
+              Answer(2, "Mid East (DE, DC, MD, NJ, NY, PA)"),
+              Answer(3, "Great Lakes (IL, IN, MI, OH, WI)"),
+              Answer(4, "Plains (IA, KS, MN, MO, NE, ND, SD)"),
+              Answer(5, "Southeast (AL, AR, FL, GA, KY, LA, MS, NC, SC, TN, VA, WV)"),
+              Answer(6, "Southwest (AZ, NM, OK, TX)"),
+              Answer(7, "Rocky Mountains (CO, ID, MT, UT, WY)"),
+              Answer(8, "Outlying Areas (AS, FM, GU, MH, MP, PR, PW, VI)")]),
+    Question(3, "What do you prefer the environment to be?",
+             [Answer(1, "City (Population: 250,000+)"),
+              Answer(2, "Suburb (Population: 100,000 - 250,000)"),
+              Answer(3, "Town (cluster 10 - 35 miles away from urban area)"),
+              Answer(4, "Rural (5 - 25 miles away from urban area)")]),
+    Question(4, "What majors/areas of study are you most interested in?",
+             [Answer(1, "STEM"),
+              Answer(2, "English/Journalism/History"),
+              Answer(3, "Fine Arts"),
+              Answer(4, "Other")]),
+    Question(5, "What degree are you looking to get in college?",
+             [Answer(1, "Associates Degree"),
+              Answer(2, "Doctorate"),
+              Answer(3, "Master's Degree"),
+              Answer(4, "Bachelor's Degree"),
+              Answer(5, "2 Year / 4 Year Special Focus")]),
 ]
 
 
 def quiz_data():
-  return questions
-
-
+    return questions
 
 # questions =
 #
