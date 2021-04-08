8c079735bebfa18f614572054be730ef20ce1694
lolabulkin <54717937+lolabulkin@users.noreply.github.com>
2021-04-07 22:22:18 -0700

    minilab html work

--- minilabs/lola/lola.py
+++ minilabs/lola/lola.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from flask import request
 from minilabs.lola.lolaminilab1 import Math
 
-minilabs_lola = Blueprint("minilabs_lola", __name__,
+minilabs_lola = Blueprint('minilabs_lola', __name__,
                           url_prefix="/lola",
                           template_folder="templates",
                           static_folder='static', static_url_path='assets')
@@ -10,4 +10,12 @@ minilabs_lola = Blueprint("minilabs_lola", __name__,
 
 @minilabs_lola.route("/minilab", methods=["GET", "POST"])
 def lola():
-    return render_template("lola/lolaminilab1.html", Math=Math)
+    if request.form == 'POST':
+        print(request.method)
+        a=int(request.form.get("a"))
+        b=int(request.form.get("b"))
+        return render_template("lola/lolaminilab1.html", Math=Math(a,b), a=a, b=b, SumDigits=Math.sum_digits, ProductDigits=Math.product_digits, product=Math.product, sum=Math.sum)
+    return render_template("lola/lolaminilab1.html")
+
+
+
