42360c9b12e89245540593bf70ea1d445a28d67c
lolabulkin <54717937+lolabulkin@users.noreply.github.com>
2021-04-08 08:09:48 -0700

    nick fix

--- minilabs/nick/nick.py
+++ minilabs/nick/nick.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from minilabs.nick.nickminilab1 import Pigeon
+from minilabs.nick.nickminilab1 import random
 
 minilabs_nick = Blueprint('minilabs_nick', __name__,
                             url_prefix="/nick",
@@ -10,4 +10,4 @@ minilabs_nick = Blueprint('minilabs_nick', __name__,
 
 @minilabs_nick.route("/minilab")
 def nick():
-    return render_template("nick/nickminilab1.html", pigeon=Pigeon(90))
+    return render_template("nick/nickminilab1.html", random=random(90))
