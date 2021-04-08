6aa8b3cc3739a205f1d5b2f99b8850597eaac85f
Nicholas Gee <@nikko22@gmail.com>
2021-04-08 14:18:07 -0700

    nickminilab1

--- minilabs/nick/nick.py
+++ minilabs/nick/nick.py
@@ -10,4 +10,4 @@ minilabs_nick = Blueprint('minilabs_nick', __name__,
 
 @minilabs_nick.route("/minilab")
 def nick():
-    return render_template("nick/nickminilab1.html", flamingo=Flamingo(20))
+    return render_template("/nick/nickminilab1.html", flamingo=Flamingo(20))
