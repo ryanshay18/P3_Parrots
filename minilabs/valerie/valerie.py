7d0d030335cbca388409c2c9bbb4e976d7be386e
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2021-04-29 21:17:54 -0700

    route val

--- minilabs/valerie/valerie.py
+++ minilabs/valerie/valerie.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template
-from flask import request
 from minilabs.valerie.valerieminilab import Pigeon
 
 minilabs_valerie = Blueprint('minilabs_valerie', __name__,
@@ -8,9 +7,6 @@ minilabs_valerie = Blueprint('minilabs_valerie', __name__,
                             static_folder='static', static_url_path='assets')
 
 
-@minilabs_valerie.route('/minilab', methods=["GET", "POST"])
+@minilabs_valerie.route('/minilab')
 def index():
-    if request.form == 'POST':
-        print(request.method)
-        return render_template("valerie/valerieminilab.html", pigeon=Pigeon(200))
-    return render_template("valerie/valerieminilab.html")
+    return render_template("valerie/valerieminilab1.html", pigeon=Pigeon(200))
