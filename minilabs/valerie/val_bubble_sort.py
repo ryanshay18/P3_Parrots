a9eceed60b3e7ac48fffa5c7d768fbf88f5d757c
valeriemiliteeva <valeriemiliteeva@valeries-air.lan>
2021-04-30 14:30:22 -0700

    val bubble sort html

--- minilabs/valerie/val_bubble_sort.py
+++ minilabs/valerie/val_bubble_sort.py
@@ -1,65 +1,81 @@
-print('welcome to the automatic bubble sorter')
-inputted_list = input('please enter a list of numbers seperated by commas: ')
-list = inputted_list.split(',')
-number_of_items = int(len(list))
 
-sorting_method = input('if you would like your list to be sorted in ascending order, press 1, if you would like it '
-                       'to be sorted in descending order, press 2')
-print(list)
+def sort(input_list, sorting_method):
 
-if sorting_method == '1':
-    position = 0
-    Pass = 0
-    counter = 1
-    while counter == 1:
-        number_of_swaps = 1
-        counter2 = 0
-        permanent_numbers = []
-        while number_of_swaps > 0:
-            counter2 = counter2 + 1
-            number_of_swaps = 0
-            while position < number_of_items - 1:
-                if int(list[position]) > int(list[position + 1]):
-                    number_of_swaps = number_of_swaps + 1
-                    item1 = int(list[position])
-                    item2 = int(list[position + 1])
-                    list[position] = item2
-                    list[position + 1] = item1
-                position = position + 1
-            Pass = Pass + 1
-            print('pass', Pass, ':', list)
-            position = 0
-            if Pass == number_of_items - 1:
-                number_of_swaps = 0
-            permanent_numbers.append(list[number_of_items - counter2])
-        if number_of_swaps == 0:
-            counter = 0
+    result = []
 
-    print('total number of passes:', Pass)
+    a_list = input_list.split(',')
+    map_object = map(int, a_list)
+    num_list = list(map_object)
 
-    if sorting_method == '2':
+    number_of_items = int(len(num_list))
+
+    if sorting_method == '1':
         position = 0
         Pass = 0
         counter = 1
         while counter == 1:
             number_of_swaps = 1
+            counter2 = 0
+            permanent_numbers = []
             while number_of_swaps > 0:
+                counter2 = counter2 + 1
                 number_of_swaps = 0
                 while position < number_of_items - 1:
-                    if int(list[position]) > int(list[position + 1]):
+                    if int(num_list[position]) > int(num_list[position + 1]):
                         number_of_swaps = number_of_swaps + 1
-                        item1 = int(list[position])
-                        item2 = int(list[position + 1])
-                        list[position] = item2
-                        list[position + 1] = item1
+                        item1 = int(num_list[position])
+                        item2 = int(num_list[position + 1])
+                        num_list[position] = item2
+                        num_list[position + 1] = item1
                     position = position + 1
                 Pass = Pass + 1
-                print('pass', Pass, ':', list)
+                print('pass', Pass, ':', num_list)
+                result.append('pass {Pass} : {num_list}'
+                              .format(Pass=Pass, num_list=num_list))
                 position = 0
-            if Pass == number_of_items - 1:
-                number_of_swaps = 0
+                if Pass == number_of_items - 1:
+                    number_of_swaps = 0
+                permanent_numbers.append(num_list[number_of_items - counter2])
+            if number_of_swaps == 0:
+                counter = 0
 
-        if number_of_swaps == 0:
-            counter = 0
+        print('total number of passes:', Pass)
 
-    print('total number of passes:', Pass)
+        if sorting_method == '2':
+            position = 0
+            Pass = 0
+            counter = 1
+            while counter == 1:
+                number_of_swaps = 1
+                while number_of_swaps > 0:
+                    number_of_swaps = 0
+                    while position < number_of_items - 1:
+                        if int(num_list[position]) > int(num_list[position + 1]):
+                            number_of_swaps = number_of_swaps + 1
+                            item1 = int(num_list[position])
+                            item2 = int(num_list[position + 1])
+                            num_list[position] = item2
+                            num_list[position + 1] = item1
+                        position = position + 1
+                    Pass = Pass + 1
+                    print('pass', Pass, ':', num_list)
+                    result.append('pass {Pass} : {num_list}'
+                              .format(Pass=Pass, num_list=num_list))
+                    position = 0
+                if Pass == number_of_items - 1:
+                    number_of_swaps = 0
+
+            if number_of_swaps == 0:
+                counter = 0
+
+        print('total number of passes:', Pass)
+
+    return result
+
+
+# print('welcome to the automatic bubble sorter')
+# inputted_list = input('please enter a list of numbers seperated by commas: ')
+#
+# sorting_method = input('if you would like your list to be sorted in ascending order, press 1, if you would like it '
+#                        'to be sorted in descending order, press 2')
+# print(list)
--- minilabs/valerie/valerie.py
+++ minilabs/valerie/valerie.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from minilabs.valerie.valerieminilab import Pigeon
+
+from minilabs.valerie.val_bubble_sort import sort
 
 minilabs_valerie = Blueprint('minilabs_valerie', __name__,
                             url_prefix="/valerie",
@@ -10,3 +12,15 @@ minilabs_valerie = Blueprint('minilabs_valerie', __name__,
 @minilabs_valerie.route('/minilab')
 def index():
     return render_template("valerie/valerieminilab1.html", pigeon=Pigeon(200))
+
+@minilabs_valerie.route('/minilab/post', methods=['POST'])
+def post():
+
+  numbers_str = request.form['numbers']
+  sorting_method = '1'
+
+  passes = sort(numbers_str, sorting_method)
+  print(passes)
+
+  return render_template("valerie/valerieminilab1-results.html",
+                         passes=passes, no_of_passes=len(passes))
