e372cfa02878f411835271fcf49742e87cac76ab
ryanshay <ryanshay88@gmail.com>
2021-04-30 23:59:08 -0700

    Minilab mine

--- minilabs/ryan/ryan.py
+++ minilabs/ryan/ryan.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from minilabs.ryan.ryanminilab import startupStack
 
@@ -11,3 +11,11 @@ minilabs_ryan = Blueprint("minilabs_ryan", __name__,
 @minilabs_ryan.route("/minilab")
 def ryan():
     return render_template("ryan/ryanminilab1.html", ideaStack = startupStack())
+
+@app.route('/ideaGiven', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        minilab1_data = request.form
+        return render_template('ideGiven.html',minilab1_data = minilab1_data)
--- minilabs/ryan/ryanminilab.py
+++ minilabs/ryan/ryanminilab.py
@@ -11,20 +11,42 @@ class startupStack:
     def setIndustry(self, input):
         self.industry = input
 
+    @property
     def getIndustry(self):
         return self.industry
-        # might need to add underscore after self
 
     def getIdea(self):
-        stack = ['Better Grocery Bulk Dispensing in Grocery Stores', 'Smart Toliet for Biometric Data', 'A vertical social network for floraphiles', 'Infrastructure to launch resaurants at lower cost', 'An Api for Multiple Real estate listing services']
+        stack = ['Better Grocery Bulk Dispensing in Grocery Stores', 'Smart Toilet for Biometric Data',
+                 'A vertical social network for floraphiles', 'Infrastructure to launch restaurants at lower cost',
+                 'An Api for Multiple Real estate listing services']
         n = random.randint(0, 3)
         return stack[n]
 
+    def sequential_search(self, alist, item):
+        position = 0
 
+        while position < len(alist):
+            if alist[position] == item:
+                print("Its there!")
+                return True
+            position = position + 1
+
+        print("No luck! :(")
+        return False
+
+
+# Initialization:
 ideaStack = startupStack()
 
+# Setter
 ideaStack.setIndustry('Tech')
+# Getter
+print("The industry chosen is: " + ideaStack.getIndustry)
 
-print("The industry chosen is: " + ideaStack.getIndustry())
-
+# Getter
 print(ideaStack.getIdea())
+
+# Algorithm
+Industries = ["Tech", "Florists", "Retail"]
+ideaStack.sequential_search(Industries, "Tech")
+ideaStack.sequential_search(Industries, "Movies")
