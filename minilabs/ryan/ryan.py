3748f313e43a7b548c902488368b282fec6b490a
ryanshay <ryanshay88@gmail.com>
2021-05-02 01:55:51 -0700

    bubblesort fix hopefully

--- minilabs/ryan/ryan.py
+++ minilabs/ryan/ryan.py
@@ -10,28 +10,24 @@ minilabs_ryan = Blueprint("minilabs_ryan", __name__,
                           static_folder='static', static_url_path='assets')
 
 
-@minilabs_ryan.route("/minilab", methods=['POST', 'GET'])
+@minilabs_ryan.route("/minilab")
 def ryan():
     return render_template("ryan/ryanminilab1.html", ideaStack=startupStack())
 
 
+@minilabs_ryan.route('/ideaGiven', methods=['POST', 'GET'])
 def bubble_data():
     bdata = request.form['text']
-    return bubbleSort(bdata)
+    bubblesorter = bubbleSort(bdata)
+    print(bubblesorter)
+    return render_template("ryan/ideaGiven.html", bubblesorter=bubblesorter)
+
 
 """
-@minilabs_ryan.route('/', methods=['POST', 'GET'])
 def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         minilab1_data = request.form
         return render_template('ideGiven.html', minilab1_data=minilab1_data)
-"""
-"""
-def bubble_data():
-    bdata = request.form['text']
-    return bubbleSort(bdata)
-
-app.jinja_env.globals.update(bubble_data=bubble_data)
 """
